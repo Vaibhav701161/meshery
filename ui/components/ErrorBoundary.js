eff6c7318cb9acae5d47f1a6098109a59d20fffb
Vaibhav Mittal <vaibhavmittal146@gmail.com>
2024-10-10 14:45:19 +0000

    Remove deprecated comment from ErrorBoundary as per review
    Signed-off-by: Vaibhav Mittal <vaibhavmittal146@gmail.com>

--- ui/components/ErrorBoundary.js
+++ ui/components/ErrorBoundary.js
@@ -5,7 +5,7 @@ import { ErrorBoundary as SistentErrorBoundary } from '@sistent/react';
 /**
  * ErrorBoundary is a React component that catches JavaScript errors in its child components and renders a fallback UI when an error occurs.
  * It should be used as a wrapper around components that might throw errors.
- * @deprecated use error boundary from sistent instead
+
  */
 const ErrorBoundary = ({ children }) => {
   return (
